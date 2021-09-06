eba82f473d8a73cdf6a13e3eb2fea07429aace44
Gabriel Moura <mourasb.gabriel@gmail.com>
2021-09-06 11:30:17 -0300

    feat: add auction interaction with DynamoDb

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -4,15 +4,24 @@ import { v4 as uuid } from 'uuid';
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
+const createAuctionAtDynamoDb = async (auction) => {
+    return dynamoDb.put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction
+    }).promise();
+};
+
 const createAuction = async (event, context) => {
     const { title } = JSON.parse(event.body);
 
     const auction = {
         title,
         id: uuid(),
-        createdAt: new Date().toDateString
+        status: 'OPEN',
+        createdAt: new Date().toISOString()
     };
 
+    await createAuctionAtDynamoDb(auction);
 
     return {
         statusCode: 201,
