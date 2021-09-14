712a469ab8b54397055e76b19f07a5e39556d1aa
Gabriel Moura <mourasb.gabriel@gmail.com>
2021-09-14 10:21:35 -0300

    feat: add middy middlewares

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,32 +1,45 @@
 
-import { DynamoDB } from 'aws-sdk';
+import middy from '@middy/core';
 import { v4 as uuid } from 'uuid';
+import { DynamoDB } from 'aws-sdk';
+import createHttpError from 'http-errors';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
 const createAuctionAtDynamoDb = async (auction) => {
-    return dynamoDb.put({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-        Item: auction
-    }).promise();
+  return dynamoDb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction
+  }).promise();
 };
 
 const createAuction = async (event, context) => {
-    const { title } = JSON.parse(event.body);
+  try {
+    const { title } = event.body;
 
     const auction = {
-        title,
-        id: uuid(),
-        status: 'OPEN',
-        createdAt: new Date().toISOString()
+      title,
+      id: uuid(),
+      status: 'OPEN',
+      createdAt: new Date().toISOString()
     };
 
     await createAuctionAtDynamoDb(auction);
 
     return {
-        statusCode: 201,
-        body: JSON.stringify(auction)
+      statusCode: 201,
+      body: JSON.stringify(auction)
     };
+
+  } catch (error) {
+    throw new createHttpError.InternalServerError(error);
+  }
 };
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler())
+  .use(httpJsonBodyParser());
